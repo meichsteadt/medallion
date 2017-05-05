88ed27d9062f46eb6f4186a47a2c51a3753f832c
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-05-05 14:14:28 -0700

    mobile styling

---
+++ nav.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.navicon').click(function() {
+    $('.mobile-nav').css('left', 0);
+    $('.nav-overlay').show();
+    $('body').css('overflow', 'hidden');
+  });
+
+  $('#close').click(function() {
+    $('.mobile-nav').css('left', '-100%');
+    $('.nav-overlay').hide();
+    $('body').css('overflow', '');
+  })
+})
